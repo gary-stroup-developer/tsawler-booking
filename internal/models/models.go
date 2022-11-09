32f8b3469e5804a1d79bb59128358abdbbecac1c
gary-stroup-developer <stroup.uxuidesign@gmail.com>
2022-11-08 19:51:15 -0800

    finish db repository and setup models that will be used to store information of users, rooms, restrictions, and reservations

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,6 +1,10 @@
 package models
 
-import "github.com/gary-stroup-developer/tsawler-booking/internal/forms"
+import (
+	"time"
+
+	"github.com/gary-stroup-developer/tsawler-booking/internal/forms"
+)
 
 // Template data holds data sent from handlers to templates
 type TemplateData struct {
@@ -20,4 +24,58 @@ type Reservation struct {
 	LastName  string
 	Email     string
 	Phone     string
+}
+
+// Users is the User model
+type Users struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+type Rooms struct {
+	ID        int
+	RoomName  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Restrictions struct {
+	ID              int
+	RestrictionName string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+type Reservations struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+	StartDate time.Time
+	EndDate   time.Time
+	RoomID    int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Room      Rooms
+}
+
+type RoomRestrictions struct {
+	ID            int
+	StartDate     time.Time
+	EndDate       time.Time
+	RoomID        int
+	ReservationID int
+	RestrictionID int
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Room          Rooms
+	Reservation   Reservations
+	Restriction   Restrictions
 }
